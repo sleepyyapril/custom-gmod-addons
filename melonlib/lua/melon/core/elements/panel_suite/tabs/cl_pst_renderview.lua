3d0ac18e29acbf310345bb8f9c7b7542c94f2c3b
sleepyyapril <flyingkarii@gmail.com>
2025-04-24 00:48:25 -0300

    remove

--- melonlib/lua/melon/core/elements/panel_suite/tabs/cl_pst_renderview.lua
+++
@@ -1,6 +0,0 @@
---@TODO
--- Hook EVERY render function here and track it, then we can display everything right :)
-
-local _PANEL = vgui.Register("Melon:PanelSuite:Tab:RenderView", {}, "DPanel")
-
-melon.DebugPanel2__TEST()
